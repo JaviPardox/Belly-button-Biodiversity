619a0953ab1079d96227891eefb2144b4cf95a11
Javier Pardo <47202605+JaviPardox@users.noreply.github.com>
2021-04-01 18:17:05 -0700

    add bar and bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,80 @@
+function charts(sample)
+{
+    d3.json("samples.json").then(function(data)
+    {
+        var samples = data.samples;
+        console.log(samples);
+
+        var result_array = samples.filter(function(data)
+        {
+            return data.id == sample;
+        });
+        console.log(result_array);
+
+        // Extract dictionary from list of objects (len = 1)
+        var result = result_array[0];
+        console.log(result);
+
+        // Dictionary notation 
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        // Build bubble chart
+        var bubble_layout = {
+            title: "Bacteria Cultures per Sample",
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"},
+            margin: {t: 30}
+        };
+
+        var bubble_data = [
+            {
+                x: otu_ids,
+                y: sample_values,
+                text: otu_labels,
+                mode: "markers",
+                marker: {
+                    size: sample_values,
+                    color: otu_ids,
+                    colorscale: "Earth"
+                }
+
+            }
+        ];
+
+        Plotly.newPlot("bubble", bubble_data, bubble_layout);
+
+        var yticks = otu_ids.slice(0,10).map(function(otu_ID)
+        {
+            return `OTU ${otu_ID}`;
+        }).reverse();
+        
+        var bar_data = [
+            {
+                y: yticks,
+                x: sample_values.slice(0,10).reverse(),
+                text: otu_labels.slice(0,10).reverse(),
+                type: "bar",
+                orientation: "h"
+            }
+        ];
+
+        var bar_layout = {
+            title: "Top bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        };
+
+        Plotly.newPlot("bar", bar_data, bar_layout);
+
+    });
+
+}
+
+function meta_data(sample)
+{
+
+}
 
 
 function init()
